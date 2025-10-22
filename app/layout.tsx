3e020b16f34ce63339973b17b3de725d4fb44c57
Surya Uppalapati <suryauppalapati9@gmail.com>
2025-10-22 15:43:02 +0530

    feat: add font space grotesk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk as Grotesk } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const grotesk = Grotesk({
+  variable: "--font-space-grotesk",
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -21,7 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`
-          ${inter.variable}
+          ${inter.className}
+          ${grotesk.variable}
           antialiased
         `}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="card-wrapper bg-dark-gradient text-light-850">Welcome to CodeOverflow!</div>;
+  return <div className="font-space-grotesk">Welcome to CodeOverflow!</div>;
 }
